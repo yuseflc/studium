8af1eb261e7b7af2084fb3f86958aacc43e884e1
vjp-darioMR <didunoxd@gmail.com>
2026-05-07 19:23:46 +0200

    [Design] Landing page update

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,9 +1,14 @@
 import Hero from "@/components/sections/Hero";
+import Features from "@/components/sections/Features";
+import Pricing from "@/components/sections/Pricing";
+import CustomersTableCard from "@/components/sections/FeaturesCard";
 
 export default function Home() {
   return (
     <main className="flex-1 bg-base-100 h-full">
       <Hero />
+      <Features />
+      <Pricing />
     </main>
   );
 }
