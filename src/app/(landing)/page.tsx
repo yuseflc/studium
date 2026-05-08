c61f913f10ae9d2e47c5f8ee744f1827b9c8ae7a
System32-0101 <didunoxd@gmail.com>
2026-05-08 17:02:33 +0200

    [Design] Creado footer y arregladas tarjetas de pricing + responsive

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -2,13 +2,14 @@ import Hero from "@/components/sections/Hero";
 import Features from "@/components/sections/Features";
 import Pricing from "@/components/sections/Pricing";
 import CustomersTableCard from "@/components/sections/FeaturesCard";
-
+import { Footer } from "@/components/sections/Footer";
 export default function Home() {
   return (
     <main className="flex-1 bg-base-100 h-full">
       <Hero />
       <Features />
       <Pricing />
+      <Footer />
     </main>
   );
 }
