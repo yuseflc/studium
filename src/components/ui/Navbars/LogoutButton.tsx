84f159edf71eb5ff9fdd67d4849b595663b11f13
System32-0101 <didunoxd@gmail.com>
2026-05-11 16:47:35 +0200

    [Fix] Boton de cerrar sesion en CourseNavbar funcionando

---
+++ src/components/ui/Navbars/LogoutButton.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { signOut } from 'next-auth/react';
+
+export default function LogoutButton() {
+  return (
+    <a onClick={() => signOut({ redirect: true })} className="cursor-pointer">
+      Logout
+    </a>
+  );
+}
