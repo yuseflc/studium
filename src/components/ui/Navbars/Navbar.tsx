be024296d50710977adecfdb544209bce1b4bf33
Eva <aveeca@gmail.com>
2026-05-31 13:03:24 +0200

    [Update] Eliminación de la opción Acerca de

--- src/components/ui/Navbars/Navbar.tsx
+++ src/components/ui/Navbars/Navbar.tsx
@@ -59,7 +59,6 @@ export default function Navbar() {
                                 <li><Link href="#top" onClick={(e) => { e.preventDefault(); document.getElementById('top')?.scrollIntoView({ behavior: 'smooth' }); document.getElementById('my-drawer-5')?.click(); }}>Inicio</Link></li>
                                 <li><Link href="#features" onClick={(e) => { e.preventDefault(); document.getElementById('features')?.scrollIntoView({ behavior: 'smooth' }); document.getElementById('my-drawer-5')?.click(); }}>Herramientas</Link></li>
                                 <li><Link href="#pricing" onClick={(e) => { e.preventDefault(); document.getElementById('pricing')?.scrollIntoView({ behavior: 'smooth' }); document.getElementById('my-drawer-5')?.click(); }}>Precios</Link></li>
-                                <li><Link href="#about" onClick={() => document.getElementById('my-drawer-5')?.click()}>Acerca de</Link></li>
                                 <div className="divider"></div>
                                 <li><Link href="/mycourses" className="btn btn-primary text-white">Mis cursos</Link></li>
                             </ul>
